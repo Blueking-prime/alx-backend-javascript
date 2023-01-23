fd9901e5ccb608af4f9deb86032dd4cb79197383
Nnamdi Anyanwu <blueking189@gmail.com>
2023-01-24 00:19:36 +0100

    Fix typos

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const [idx, val] of array.entries()) {
-    array[idx] = appendString + val;// eslint-disable-line no-param-reassign
+    array[idx] = appendString + val; // eslint-disable-line no-param-reassign
   }
 
   return array;
