c0c9bc60c3c4943238f9ff4f466333340e85dc78
Nnamdi Anyanwu <blueking189@gmail.com>
2023-01-24 00:14:42 +0100

    More Bug and ESlint  fixes

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const arr = array;
-  for (const idx of arr) {
-    const value = arr[idx];
-    arr[idx] = appendString + value;
+  for (const [idx, val] of array.entries()) {
+    array[idx] = appendString + val;// eslint-disable-line no-param-reassign
   }
 
-  return arr;
+  return array;
 }
