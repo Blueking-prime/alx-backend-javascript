98bab1ddac0bfc6cd2aa105265fdcfe242ad3bab
Nnamdi Anyanwu <blueking189@gmail.com>
2023-01-25 01:56:57 +0100

    Checker test 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,5 +2,8 @@ export default function handleResponseFromAPI(promise) {
   promise.then(() => ({
     status: 200,
     body: 'success',
-  }), () => Error).then(console.log('Got a response from the API'));
+  }), () => Error);
+  promise.finally(console.log('Got a response from the API'));
+
+  return promise
 }
