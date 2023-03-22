4a92d2135c595a3169a54299ec557211191034f7
Nnamdi Anyanwu <blueking189@gmail.com>
2023-03-22 20:37:36 +0100

    ESlint fixes

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,27 +1,27 @@
-function countStudents (path) {
-  const fs = require('fs');
+const fs = require('fs');
 
-  fs.readFile(path, 'utf-8', function (err, data) {
+function countStudents(path) {
+  fs.readFile(path, 'utf-8', (err, data) => {
     if (err) {
       throw new Error('Cannot load the database');
     }
     const dataList = data.trim().split('\n');
     dataList.shift();
-    console.log('Number of students: %d', dataList.length);
+    console.log(`Number of students: ${ dataList.length}`);
 
     const fields = new Set();
-    for (let i = 0; i < dataList.length; i++) {
+    for (let i = 0; i < dataList.length; i += 1) {
       dataList[i] = dataList[i].split(',');
       fields.add(dataList[i][3]);
     }
     for (const field of [...fields]) {
       const fieldList = [];
-      for (let i = 0; i < dataList.length; i++) {
+      for (let i = 0; i < dataList.length; i += 1) {
         if (dataList[i][3] === field) {
           fieldList.push(dataList[i][0]);
         }
       }
-      console.log('Number of students in %s: %d. List: %s', field, fieldList.length, fieldList.join(', '));
+      console.log(`Number of students in ${field}: ${fieldList.length}. List: ${fieldList.join(', ')}`);
     }
   });
 }
